0e5e2fe4c3c4bc81ef1b7ae11135861871b1dd81
toby <topias.pyykkonen@helsinki.fi>
2023-11-14 20:54:40 +0200

    remove push & pop pattern

--- src/Optimiser.cpp
+++ src/Optimiser.cpp
@@ -96,25 +96,101 @@ vector<string> use_register_instead_of_variable(vector<string> instructions, vec
     return instructions;
 }
 
+string find_first_operand(string instruction) {
+    
+    size_t space_pos = instruction.find_first_of(" ");
+    size_t comma_pos = instruction.find_first_of(",");
+    
+    if (space_pos == std::string::npos) {
+        throw std::runtime_error("Could not find first operand!");
+    }
 
+    return instruction.substr(space_pos + 1, comma_pos - space_pos - 1);
+
+
+}
+
+
+static inline string ltrim(std::string s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+    return s;
+}
+
+vector<string> remove_push_pop_pattern(vector<string> instructions) {
+    // Quite commonly the same register is pushed on to the stack and immediately popped back
+    // Remove all such patterns.
+    // Example:
+    //      push r8
+    //      pop r8
+
+    if (instructions.size() < 2) {
+        // Very odd corner case
+        return instructions;
+    }
+
+    vector<string> rtrn;
+    std::vector<bool> include(instructions.size(), true);
+
+    string prev, cur;
+    for (int i = 1; i < instructions.size(); i++) {
+        
+        prev = instructions[i-1];
+        cur  = instructions[i];
+
+        if (prev.size() < 5) {
+            continue;
+        }
+        if (cur.size() < 4) {
+            continue;
+        }
+
+        string reg1 = ltrim(prev);
+        string reg2 = ltrim(cur);
+
+        if (reg1.rfind("push", 0) == 0) {
+            if (reg2.rfind("pop ", 0) == 0) {
+                if (find_first_operand(reg1) == find_first_operand(reg2)) {
+                    include[i-1] = false;
+                    include[i] = false;
+                }
+            }
+        }
+
+    }
+    
+    for (int i = 1; i < instructions.size(); i++) {
+        if (include[i]) {
+            rtrn.push_back(instructions[i]);
+        }
+        
+    }
+
+    return rtrn;
+}
 
 
 vector<string> optimise(vector<string> instructions) {
     
     std::clog << "Optimising!\n";
+    // std::clog << "operand: " << find_first_operand("push r8") << "\n";
 
     set<string>     regs = find_registers(instructions);
     vector<string>  vars = get_variables(instructions);
 
 
-    for (auto v: vars) {
-        std::clog << v << ", ";
-    }
-    std::clog << "\n";
-    std::for_each(regs.cbegin(), regs.cend(), [](string r) {
-        std::clog << "" << r << ", ";
-    });
-    std::clog << "\n";
+
+    instructions = remove_push_pop_pattern(instructions);
+
+    // for (auto v: vars) {
+    //     std::clog << v << ", ";
+    // }
+    // std::clog << "\n";
+    // std::for_each(regs.cbegin(), regs.cend(), [](string r) {
+    //     std::clog << "" << r << ", ";
+    // });
+    // std::clog << "\n";
 
     
     return instructions;
